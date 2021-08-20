34a411c4fd899ce94a53b400f7c1b479000088a1
Ankur Saha <ankursahamoto@gmail.com>
2021-08-20 21:58:44 +0530

    Greedy update

--- Greedy/MinimumPlatforms.cpp
+++ Greedy/MinimumPlatforms.cpp
@@ -2,6 +2,8 @@
 // Linkedin: https://www.linkedin.com/in/ankur-saha/
 // Github: https://github.com/Excalibur79
 
+// Two pointers approach
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,7 +21,22 @@ const ll INF = 1e18;
 #define printarr(arr, n) \
     for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
 
-void solve() {}
+void solve() {
+    int arr[] = {900, 940, 950, 1100, 1500, 1800};
+    int dept[] = {910, 1200, 1120, 1130, 1900, 2000};
+    int n = 6;
+    sort(arr, arr + n);
+    sort(dept, dept + n);
+    int minPlatforms = 1;
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] <= dept[j])
+            minPlatforms++;
+        else
+            j++;
+    }
+    cout << minPlatforms << endl;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
