6ba0c2b2cb6e708c5b5e2674f21472a97a7bef59
Ankur Saha <ankursahamoto@gmail.com>
2021-08-25 01:22:11 +0530

    backtracking update

---
+++ Backtracking/StringPermutations.cpp
@@ -0,0 +1,49 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void permute(string s, int i) {
+    if (i == s.length()) {
+        cout << s << endl;
+    }
+    for (int j = i; j < s.length(); j++) {
+        swap(s[i], s[j]);
+        permute(s, i + 1);
+        swap(s[i], s[j]);  // backtracking to restore this level string
+    }
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    permute(s, 0);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
