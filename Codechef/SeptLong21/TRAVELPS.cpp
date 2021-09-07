078a6ab17ea98f98a51345db0364f9dc4eb3c467
Ankur Saha <ankursahamoto@gmail.com>
2021-09-07 19:34:06 +0530

    sept codechef longchallenge update

---
+++ Codechef/SeptLong21/TRAVELPS.cpp
@@ -0,0 +1,48 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (char c : s) {
+        if (c == '0')
+            ans += a;
+        else
+            ans += b;
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
