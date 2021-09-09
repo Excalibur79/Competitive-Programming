5a0c41af576e599168a6f5d1ce205f9fd9c33d03
Ankur Saha <ankursahamoto@gmail.com>
2021-09-09 15:35:13 +0530

    contest upsolve update

--- Codeforces/EducationalRound113/Chesstournament.cpp
+++ Codeforces/EducationalRound113/Chesstournament.cpp
@@ -25,15 +25,54 @@ void solve() {
     cin >> n;
     string s;
     cin >> s;
-    vector<vector<char>> matrix(n, vector<int>(n, '.'));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) matrix[i][j] = 'X';
+    vector<vector<char>> matrix(n, vector<char>(n, '.'));
+    // observation  => losing when number of 2s is either 1 or 2
+    int ones = 0, twos = 0;
+    for (char c : s) {
+        if (c == '2')
+            twos++;
+        else
+            ones++;
+    }
+    if (twos == 1 || twos == 2) {
+        cout << "No" << endl;
+
+    } else {
+        cout << "Yes" << endl;
+        // concept if s[i] is 1 then lets make matches with all s[j] as draw ie.
+        // '= if s[i] is 2 and s[j] is also 2 then  make i the winner and sest
+        // satisfied true which idicates that i may loose other matches
+
+        for (int i = 0; i < n; i++) {
+            bool satisfied = false;
+            for (int j = i; j < n; j++) {
+                if (i == j)
+                    matrix[i][j] = 'X';
+                else {
+                    if (s[i] == '1' || s[j] == '1') {
+                        matrix[i][j] = '=';
+                        matrix[j][i] = '=';
+                    }
+
+                    if (s[i] == '2' && s[j] == '2' && !satisfied) {
+                        matrix[i][j] = '+';
+                        matrix[j][i] = '-';
+                        satisfied = true;
+                    } else if (s[i] == '2' && s[j] == '2' && satisfied) {
+                        matrix[i][j] = '-';
+                        matrix[j][i] = '+';
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j];
+            }
+            cout << endl;
         }
     }
-    // first filling 1s
-
-    //================
 }
 
 int32_t main() {
