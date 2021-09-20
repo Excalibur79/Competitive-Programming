06f8a3e7a0622965c6325ae745a9dc3f0ebb359d
Ankur Saha <ankursahamoto@gmail.com>
2021-09-20 16:08:30 +0530

    trees update

---
+++ Trees/DiameterOFTree.cpp
@@ -0,0 +1,65 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+int maxHeight = 0;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+int diameter = 0;
+
+int diameterOfTree(TreeNode *node) {
+    if (!node) return 0;
+    int lh = diameterOfTree(node->left);
+    int rh = diameterOfTree(node->right);
+    diameter = max(diameter, lh + rh);
+    return max(lh, rh) + 1;
+}
+
+void solve() {
+    struct TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    diameterOfTree(root);
+    cout << diameter << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
