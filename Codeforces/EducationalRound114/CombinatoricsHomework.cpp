e9423b344d1cae81b7dab56b6af6830fb9d8226f
Ankur Saha <ankursahamoto@gmail.com>
2021-09-23 11:15:56 +0530

    contest upsolve update

---
+++ Codeforces/EducationalRound114/CombinatoricsHomework.cpp
@@ -0,0 +1,47 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    vi arr(3);
+    inputarr(arr, 3);
+    int m;
+    cin >> m;
+    sort(arr.begin(), arr.end());
+    int maxx = arr[0] + arr[1] + arr[2] - 3;
+    int minn = max(0, arr[2] - arr[1] - arr[0] - 1);
+    if (m >= minn && m <= maxx)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
