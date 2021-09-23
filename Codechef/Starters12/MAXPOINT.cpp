2517be1147d09f0cf5ad57b9686a72b9278c6370
Ankur Saha <ankursahamoto@gmail.com>
2021-09-23 10:46:04 +0530

    contest upsolve

--- Codechef/Starters12/MAXPOINT.cpp
+++ Codechef/Starters12/MAXPOINT.cpp
@@ -20,46 +20,21 @@ const ll INF = 1e18;
 #define printarr(arr, n) \
     for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
 
-bool comp(vector<double> &a, vector<double> &b) { return a[0] > b[0]; }
-
-int evaluate(auto &data) {
-    // for (auto value : data)
-    //     cout << value[0] << " " << value[1] << " " << value[2] << endl;
-    int leftTime = 240;
-    int ans = 0;
-    for (auto value : data) {
-        int points = value[1];
-        int duration = value[2];
-        if (duration * 20 <= leftTime) {
-            leftTime -= duration * 20;
-            ans += 20 * points;
-        } else {
-            ans += points * (leftTime / duration);
-            leftTime = leftTime % duration;
-        }
-        // cout << "Time :" << leftTime << " points:" << ans << endl;
-    }
-    return ans;
-}
-
 void solve() {
-    vector<double> time(3);
-    vector<double> marks(3);
+    vector<int> time(3);
+    vector<int> marks(3);
     inputarr(time, 3);
     inputarr(marks, 3);
-    vector<vector<double>> data;
-    for (int i = 0; i < 3; i++) {
-        vector<double> p = {(marks[i] * 1.0) / (time[i] * 1.0), marks[i],
-                            time[i]};
-        data.pb(p);
+    int ans = INT_MIN;
+    for (int i = 0; i <= 20; i++) {
+        for (int j = 0; j <= 20; j++) {
+            for (int k = 0; k <= 20; k++) {
+                if (time[0] * i + time[1] * j + time[2] * k <= 240)
+                    ans = max(ans, marks[0] * i + marks[1] * j + marks[2] * k);
+            }
+        }
     }
-    int res = INT_MIN;
-    for (int i = 1; i <= 6; i++) {
-        res = max(res, evaluate(data));
-        next_permutation(data.begin(), data.end());
-    }
-
-    cout << res << endl;
+    cout << ans << endl;
 }
 
 int32_t main() {
