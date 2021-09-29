e8567babef6503b79ce4802c912f94791b26175e
Ankur Saha <ankursahamoto@gmail.com>
2021-09-29 19:10:57 +0530

    trees update

---
+++ Trees/LevelOrderTraversal.cpp
@@ -0,0 +1,66 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+vector<vector<int>> ans;
+
+void levelOrderTraversal(struct TreeNode *node) {
+    queue<struct TreeNode *> q;
+    q.push(node);
+    while (!q.empty()) {
+        struct TreeNode *x = q.front();
+        cout << x->val << " ";
+        q.pop();
+        if (x->left) q.push(x->left);
+        if (x->right) q.push(x->right);
+    }
+}
+
+void solve() {
+    struct TreeNode *root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    levelOrderTraversal(root);
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
