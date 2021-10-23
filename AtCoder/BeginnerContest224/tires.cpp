5d959d316f0b9226b9e2e8469e82de7092b710ac
Ankur Saha <ankursahamoto@gmail.com>
2021-10-23 19:25:22 +0530

    atcoder contest update

---
+++ AtCoder/BeginnerContest224/tires.cpp
@@ -0,0 +1,43 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    string s;
+    cin >> s;
+    int length = s.length();
+    if (s[length - 2] == 'e' && s[length - 1] == 'r')
+        cout << "er" << endl;
+    else
+        cout << "ist" << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
