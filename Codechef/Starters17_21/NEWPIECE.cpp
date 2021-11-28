a8e164fca10b371ea4622da3cb59c325f36985a8
Ankur Saha <ankursahamoto@gmail.com>
2021-11-28 11:08:35 +0530

    Starters17

---
+++ Codechef/Starters17_21/NEWPIECE.cpp
@@ -0,0 +1,45 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+void solve() {
+    int a, b, p, q;
+    cin >> a >> b >> p >> q;
+    if (a == p && b == q)
+        cout << 0 << endl;
+    else if (((a + b) % 2 == 0 && (p + q) % 2 == 0) ||
+             ((a + b) % 2 != 0 && (p + q) % 2 != 0))
+        cout << 2 << endl;
+    else
+        cout << 1 << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
