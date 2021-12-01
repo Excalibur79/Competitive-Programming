fceae3ddb9ec9f0e49a6f2fa7e1d2d7ac01cd739
Ankur Saha <ankursahamoto@gmail.com>
2021-12-01 22:57:34 +0530

    dp[ update

---
+++ Dp/coinChange.cpp
@@ -0,0 +1,51 @@
+// Author: Ankur Saha
+// Linkedin: https://www.linkedin.com/in/ankur-saha/
+// Github: https://github.com/Excalibur79
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+#define inputarr(arr, n) \
+    for (ll i = 0; i < n; i++) cin >> arr[i];
+#define printarr(arr, n) \
+    for (ll i = 0; i < n; i++) cout << arr[i] << ' ';
+
+int coinChange(vector<int> &coins, int n, int sum) {
+    if (sum == 0) return 1;
+    if (n == 0) return 0;
+    if (coins[n - 1] <= sum)
+        return coinChange(coins, n, sum - coins[n - 1]) +
+               coinChange(coins, n - 1, sum);
+    return coinChange(coins, n - 1, sum);
+}
+
+void solve() {
+    int n, sum;
+    cin >> n >> sum;
+    vector<int> coins(n);
+    inputarr(coins, n);
+    int ans = coinChange(coins, n, sum);
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
