33aa4fc3e3fcef13945194b7bcdc6e25489f3d52
Excalibur79 <ankursahamoto@gmail.com>
2022-07-01 09:33:23 +0530

    latest changes

--- x.cpp
+++ x.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double fun1(int n) {
-    double ans = 0;
-    for (int i = 1; i <= n; i++) {
-        ans += ((1 * 1.0) / i);
-    }
-    return ans;
-}
-
-double fun2(int n) { return log(n); }
-
 int main() {
-    // int i = 0;
-    // cout << i << " " << i++ << endl;
-    cout << fun1(55) << endl;
-    cout << fun2(55) << endl;
+    vector<int> arr = {};
+    arr.insert(arr.begin() + 0, 5);
+    for (int data : arr) cout << data << " ";
+    cout << endl;
+    cout << arr.size() << endl;
     return 0;
 }
